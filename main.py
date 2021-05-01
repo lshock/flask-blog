5cddca1e16326db30657563470e7d144061375d7
Liz Shockey <lizsdev@gmail.com>
2021-05-01 18:48:10 -0400

    test fix

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 #DECORATORS - FOR AUTHORIZED ACCESS
 def admin_only(f):
